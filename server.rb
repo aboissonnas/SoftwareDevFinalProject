db4ade9f23cfb63887210ff4c8a213f62d3089aa
Ariane Boissonnas <ariane@boissonnas.net>
2017-11-27 14:04:57 -0600

    set up database

---
+++ server.rb
@@ -0,0 +1,41 @@
+require "sequel"
+
+#set up the database
+configure do
+
+    #connect to the sqlite database
+    DB = Sequel.connect("sqlite://Monsters.db")
+
+    #create the monsters table
+    DB.create_table? :monsters do
+
+        primary_key :id
+        String :name
+        String :description
+        int :hit_points
+        int :ac
+        int :fortitude
+        int :reflex
+        int :will
+        int :strength
+        int :dexterity
+        int :constitution
+        int :intelligence
+        int :wisdom
+        int :charisma
+    end
+
+    #create the environments table
+    DB.create_table? :environments do
+
+        primary_key :id
+        String :name
+        String :description
+    end
+
+    #create the join table for the many-to-many relationship between monsters and environments
+    if not DB.table_exists? :monsters_environments
+
+        DB.create_join_table :monster_id => :monsters, :environment_id => :environments
+    end
+end
