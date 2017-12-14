d46114e840875035e9b043367a933a0dc04954f7
Ariane Boissonnas <ariane@boissonnas.net>
2017-12-14 15:28:06 -0600

    updated ArianeWorking

--- server.rb
+++ server.rb
@@ -14,8 +14,9 @@ def read_monster_file(filepath)
             #split the line into an array of data
             monster_data = line.split(",")
             
-            #if the monster doesn't already exist
             monster = Monster[:name => monster_data.at(0)]
+
+            #if the monster doesn't already exist
             if monster.nil?
                 #populate a monster model with the data
                 monster = Monster.create(:name => monster_data.at(0),
@@ -32,16 +33,14 @@ def read_monster_file(filepath)
                                          :wisdom => monster_data.at(11).to_i,
                                          :charisma => monster_data.at(12).to_i,
                                          :xp => monster_data.at(13).to_i)
-                        
-                #for each environment that the monster has
-                for i in 14...monster_data.length do
-          
-                    #grab the environment
+
+                for i in 14...(monster_data.length - 1) do
+
                     environ = Environment[:name => monster_data.at(i)]
            
                     #if the environment doesn't already exist
                     if environ.nil?
-            
+                        
                         #create the environment
                         environ = Environment.create(:name => monster_data.at(i))
                     end
@@ -116,4 +115,22 @@ end
 get '/' do
     @environs = Environment.all
     erb :home
+end
+
+post '/' do
+    environmentalMonsters = Environment[:id => params[:environment]]
+   # puts environmentalMonsters.id
+    envimonsters = environmentalMonsters.monsters
+    #puts envimonsters[0].id
+    eligible = Array.new()
+    #puts "entering each loop"
+    envimonsters.each() do |m|
+        eligible << Monster.where(:id => m.id)
+     #   puts m.id
+    end
+    #puts "testing this dataset"
+    monsters = eligible.map do |m|
+       monst_info = {:mid => m.first.id, :name => m.first.name, :xp => m.first.xp, :desc => m.first.description}
+    end
+    return json monsters
 end
