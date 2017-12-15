7e1d5f2bc4d0bc7b16453588993fdeff86a5efff
DASchechtman <dschechtman@carthage.edu>
2017-12-14 22:20:58 -0600

    added input type checking
    now a user can only enter the appropreate type of input for the data asked for by the program

--- Monster writer.py
+++ Monster writer.py
@@ -9,13 +9,35 @@ def validInputEnteredAt(stats, messege, index):
     while stats[last_element] == "":
         print("Please enter valid input")
         stats[last_element] = raw_input(messege)
+        
+def validTypedInputEntered(stats, message, index, flag):
+    if flag == "i":
+        while True:
+            try:
+                int(stats[index])
+                break
+            except ValueError:
+                print("enter a numerical value")
+                stats[index] = raw_input(message)
+    elif flag == "s":
+        while True:
+            try:
+                int(stats[index])
+                print("enter text input")
+                stats[index] = raw_input(message)
+            except ValueError:
+                if stats[index] != "":
+                    break
+                else:
+                    print("enter valid input")
+                    stats[index] = raw_input(message)
 
 def getEnvironments(stats):
     print("enter 'stop' to stop inputting environments")
     e = raw_input("enter environment: ")
     while e.lower() != "stop":
         stats.append(e)
-        validInputEntered(stats, "enter environment: ")
+        validTypedInputEntered(stats, "enter environment: ", len(stats)-1, "s")
         e = raw_input("enter environment: ")
 
 def getMonsterStats(stats):
@@ -23,9 +45,14 @@ def getMonsterStats(stats):
          "reflex", "will", "strength", "dexterity",
          "constitution", "intelligence", "wisdom", "charisma",
          "xp"]
+    i = 0
     for labels in s:
         stats.append(raw_input("enter %s: " %labels))
-        validInputEntered(stats, "enter %s: " %labels)
+        if i > 1:
+            validTypedInputEntered(stats, "enter %s: " %labels, len(stats)-1, "i")
+        else:
+            validTypedInputEntered(stats, "enter %s: " %labels, len(stats)-1, "s")
+        i += 1
 
 def checkInput(stats):
     s = ["name", "description", "hit points", "armor class", "fortitude",
@@ -73,11 +100,14 @@ def makeCorrectionsToInput(stats):
             if int(choice) < len(s):
                 message = "enter %s: " % s[int(choice)]
                 stats[int(choice)] = raw_input(message)
-                validInputEnteredAt(stats, message, int(choice))
+                if int(choice) > 1:
+                    validTypedInputEntered(stats, message, int(choice), "i")
+                else:
+                    validTypedInputEntered(stats, message, int(choice), "s")
             else:
                 message = "enter environment: "
                 stats[int(choice)] = raw_input(message)
-                validInputEnteredAt(stats, message, int(choice))
+                validTypedInputEntered(stats, message, int(choice), "s")
         
         pick_again = raw_input("want to change another stat? Y/N: ").lower()
 
