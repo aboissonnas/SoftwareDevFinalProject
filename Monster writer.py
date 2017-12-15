9870ec5fca71c5a66418d10bfa4910d8e83b87ca
DASchechtman <dschechtman@carthage.edu>
2017-12-14 21:29:27 -0600

    Made changes to a bunch of things
    Monster Writer.py: I made is so the user can make changes to input if they made a mistake, allows the user to check what they entered, and doesn't allow for blank input to be added
    Monster.txt: added monster
    Monster.db: tested the server, and it's there because of that

--- Monster writer.py
+++ Monster writer.py
@@ -1,8 +1,21 @@
+def validInputEntered(stats, messege):
+    last_element = len(stats) - 1
+    while stats[last_element] == "":
+        print("Please enter valid input")
+        stats[last_element] = raw_input(messege)
+
+def validInputEnteredAt(stats, messege, index):
+    last_element = index
+    while stats[last_element] == "":
+        print("Please enter valid input")
+        stats[last_element] = raw_input(messege)
+
 def getEnvironments(stats):
     print("enter 'stop' to stop inputting environments")
     e = raw_input("enter environment: ")
     while e.lower() != "stop":
         stats.append(e)
+        validInputEntered(stats, "enter environment: ")
         e = raw_input("enter environment: ")
 
 def getMonsterStats(stats):
@@ -12,6 +25,62 @@ def getMonsterStats(stats):
          "xp"]
     for labels in s:
         stats.append(raw_input("enter %s: " %labels))
+        validInputEntered(stats, "enter %s: " %labels)
+
+def checkInput(stats):
+    s = ["name", "description", "hit points", "armor class", "fortitude",
+         "reflex", "will", "strength", "dexterity",
+         "constitution", "intelligence", "wisdom", "charisma",
+         "xp"]
+    print("\nAre these the correct input? Y/N")
+    index = 0
+    for i in range(len(s)):
+        print(s[i] + " : " + stats[i])
+        index += 1
+    start_envir = index
+    end_envir = index + (len(stats)-index)
+    for i in range(start_envir, end_envir):
+        print("environment : "+stats[i])
+    return raw_input()
+
+
+def makeCorrectionsToInput(stats):
+    s = ["name", "description", "hit points", "armor class", "fortitude",
+         "reflex", "will", "strength", "dexterity",
+         "constitution", "intelligence", "wisdom", "charisma",
+         "xp"]
+    pick_again = "y"
+    while pick_again == "y":
+        print("which input do you want to fix?")
+        
+        for i in range(len(stats)):
+            if i < len(s):
+                print(str(i) + " (" + s[i] + " : " + stats[i] + ")")
+            else:
+                print(str(i) + " (environment : " + stats[i] + ")")
+        print(str(len(stats)) + " - exit")
+        
+        choice = raw_input()
+        
+        while int(choice) < 0:
+            print("please enter a valid option")
+            choice = raw_input()
+            
+        if int(choice) >= len(stats):
+            return
+        else:
+            message = ""
+            if int(choice) < len(s):
+                message = "enter %s: " % s[int(choice)]
+                stats[int(choice)] = raw_input(message)
+                validInputEnteredAt(stats, message, int(choice))
+            else:
+                message = "enter environment: "
+                stats[int(choice)] = raw_input(message)
+                validInputEnteredAt(stats, message, int(choice))
+        
+        pick_again = raw_input("want to change another stat? Y/N: ").lower()
+
 
 def writeToFile(file_path, stats):
     stuff_to_write = ""
@@ -56,5 +125,11 @@ while True:
     stats = []
     getMonsterStats(stats)
     getEnvironments(stats)
+    if checkInput(stats).lower() == "y":
+        print("adding monster to file")
+    else:
+        print("please change the input")
+        makeCorrectionsToInput(stats)
+
     writeToFile(file_path, stats)
     orderFile(file_path)
